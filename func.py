2e81661c946cee79981425043b7a8fad2c302407
LeeJunHyuk <leejh10003@gmail.com>
2015-10-26 15:05:27 +0900

    First branch commit

--- func.py
+++ func.py
@@ -1,1 +1,7 @@
-
+def maxDivision(former, latter):
+    ret = 1
+    end = min(former, latter)
+    for i in range(1, max + 1):
+        if (former % i == 0 and latter % i == 0):
+            ret = i
+    return i
