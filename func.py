942fc8f15b9298cfa2587b963fb9100acae33399
LeeJunHyuk <leejh10003@gmail.com>
2015-11-02 17:37:07 +0900

    Last Week Sample. Start from here

--- func.py
+++
@@ -1,7 +0,0 @@
-def maxDivision(former, latter):
-    ret = 1
-    end = min(former, latter)
-    for i in range(1, end + 1):
-        if (former % i == 0 and latter % i == 0):
-            ret = i
-    return ret
